236c9317ae45911f234d3111563c5c530610fc3d
Changze Han <changze-han@uiowa.edu>
2018-12-03 15:54:22 -0600

    Enable buttons to create Kids Table in the database

--- app.py
+++ app.py
@@ -43,38 +43,103 @@ class app(tk.Tk):
 
         frame = self.frames[page]
         frame.tkraise()
-
-    def ConnectDataBase(self): # function to connect the app database
+################ functions for database starts here: 
+    def ConnectDataBase(): # function to connect the app database
         #conn = sqlite3.connect('/Users/Han/Documents/CS 4980 Child-Computer Interaction(2018 FALL)/Child_Computer_Interaction_Project/AppData.db')
         conn = sqlite3.connect('AppData.db')
         print("Opened database successfully")
 
         conn.close()
 
-    def CreateKidsTable(self):
+
+    def CreateKidsTable():
         #conn = sqlite3.connect('/Users/Han/Documents/CS 4980 Child-Computer Interaction(2018 FALL)/Child_Computer_Interaction_Project/AppData.db')
         conn = sqlite3.connect('AppData.db')
         conn.execute('''CREATE TABLE if not exists Kids
-            (name text)''')
-        print ("Kids Table created successfully")
+            (number INT, name TEXT, age INT)''')
+        print ("Kids Table created successfully or already exists")
         conn.close()
+
+    #def CreateKidsTable(self):
+    #   conn = sqlite3.connect('AppData.db')
+    #    try:
+    #        conn.execute('''CREATE TABLE Kids
+    #        (name text)''')
+    #        print ("Kids Table created successfully")
+    #    except sqlite3.IntegrityError:
+    #        print("Kids Table already exists")
+
+    def InsertNewKid(self,number,name,age):
+        conn = sqlite3.connect('AppData.db')
+        with conn:
+            cur = conn.cursor()
+            cur.execute("SELECT * from Kids")
+            rows = cur.fetchall()
+            numList = []
+            for row in rows:
+                numList.append(row[0])
+            if number not in numList:
+                Kids_sql = "INSERT INTO Kids (number,name,age) VALUES (?,?,?)"
+                cur.execute(Kids_sql,(number, name, age))
+                print("Successfully insert "+ name + " into Kids Table")
+            else:
+                print("There's already a kid registered in this page. You can only update the name. ")
+        conn.close()
+
+    def UpdateKid():
+        pass
+    
+    def DeleteKid():
+        pass
+
+    def GetKidNameAge(self,num): # input the kid number, if the number exists then get the name
+        conn = sqlite3.connect('AppData.db')
+        with conn:
+            cur = conn.cursor()
+            curname = conn.cursor()
+            curage = conn.cursor()
+            cur.execute("SELECT * from Kids")
+            rows = cur.fetchall()
+            numberList = []
+            for row in rows:
+                numberList.append(row[0])
+            if num in numberList:
+                curname.execute('SELECT name from Kids WHERE number = '+str(num))
+                curage.execute('SELECT age from Kids WHERE number = '+str(num))
+                name = curname.fetchone()
+                age = curage.fetchone()
+                return ([name[0],age[0]])
+            else:
+                default = ["Please Type your name","Please Type your age"]
+                return (default)
+         
 
     def CreateChoreTable(self):
         #conn = sqlite3.connect('/Users/Han/Documents/CS 4980 Child-Computer Interaction(2018 FALL)/Child_Computer_Interaction_Project/AppData.db')
         conn = sqlite3.connect('AppData.db')
         conn.execute('''CREATE TABLE if not exists Chore
             (Chore text, time text, status text)''')
-        print ("Chore Table created successfully")
+        print ("Chore Table created successfully or already exists")
         conn.close()
+        
+    def InsertNewChore(self):
+        pass
+
+    def CreateBankTable(self):
+        #conn = sqlite3.connect('/Users/Han/Documents/CS 4980 Child-Computer Interaction(2018 FALL)/Child_Computer_Interaction_Project/AppData.db')
+        conn = sqlite3.connect('AppData.db')
+        conn.execute('''CREATE TABLE if not exists Bank
+            (name text, figure text)''')
+        print ("Bank Table created successfully or already exists")
+        conn.close()
+
+app.ConnectDataBase()
+app.CreateKidsTable()
 
 
 class StartPage(tk.Frame):  # this is the start page for the app
     
-    
-    
     def __init__(self, parent, controller):
-
-        controller.ConnectDataBase()
 
         tk.Frame.__init__(self,parent)
         
@@ -89,12 +154,11 @@ class StartPage(tk.Frame):  # this is the start page for the app
                             command = lambda: controller.show_frame(PageParents))
         button2.pack(pady = 50, padx = 10)
 
+app.ConnectDataBase()
 
 class PageAllKids(tk.Frame): # Kids select their own page (for multiple children family use)
 
     def __init__(self, parent, controller):
-
-        controller.CreateKidsTable()
         
         tk.Frame.__init__(self, parent)
         label = tk.Label(self, text = "Find your page", font = LARGE_FONT)
@@ -116,30 +180,51 @@ class PageAllKids(tk.Frame): # Kids select their own page (for multiple children
                             command = lambda: controller.show_frame(StartPage))
         button4.pack()
 
+app.ConnectDataBase()
+
 class PageChildren(tk.Frame): # Children main page
-    def changeName(label,name):
-        label['text'] = "Welcome " + name
-        
-    def getName(textBox):
-        name = textBox.get()
-        return (name)
+    def changeInfo(labelx,labely,name,age):
+        labelx['text'] = "Welcome " + name
+        labely['text'] = "Age: " + str(age)
+    def getInput(textBox):
+        Input = textBox.get()
+        return (Input)
 
     def __init__(self, parent, controller):
+
+        app.ConnectDataBase()
         
         
         tk.Frame.__init__(self, parent)
 
         #Logo = tk.PhotoImage(file = "images.gif")
+
         
-        label = tk.Label(self, text = "Please Type your name", font = LARGE_FONT)
-        label.pack(pady = 10, padx = 10)
+        label1 = tk.Label(self, text = "Welcome "+controller.GetKidNameAge(1)[0], font = LARGE_FONT)
+        label1.pack(pady = 10, padx = 10)
 
         nameBox = tk.Entry(self)
         nameBox.pack()
-        
-        button1 = tk.Button(self, text = "Change Name",
-                            command = lambda:PageChildren.changeName(label,PageChildren.getName(nameBox)))
-        button1.pack()
+
+        label2 = tk.Label(self, text = "Age: "+ str(controller.GetKidNameAge(1)[1]), font = LARGE_FONT)
+        label2.pack(pady = 10, padx = 10)
+
+        ageBox = tk.Entry(self)
+        ageBox.pack()
+
+        RefreshButton = tk.Button(self,text = "refresh",
+                                  command = lambda:PageChildren.changeInfo(label1,label2,
+                                                                           controller.GetKidNameAge(1)[0],controller.GetKidNameAge(1)[1]))
+        RefreshButton.pack()
+
+        #button1 = tk.Button(self, text = "Click to change your Infor",
+        #                    command = lambda:PageChildren.changeInfo(label1,label2,
+                                                                     #PageChildren.getInput(nameBox),PageChildren.getInput(ageBox)))
+        #button1.pack()
+
+        buttonSql = tk.Button(self, text = "Update Database",
+                            command = lambda:controller.InsertNewKid(1,PageChildren.getInput(nameBox),PageChildren.getInput(ageBox)))
+        buttonSql.pack()
 
         button2 = tk.Button(self, text = "Chores",
                             command = lambda: controller.show_frame(PageChores))
--- testSQL.py
+++ testSQL.py
@@ -8,10 +8,41 @@ def ConnectDataBase():
 
 def CreateChoreTable():
     conn = sqlite3.connect('/Users/Han/Documents/CS 4980 Child-Computer Interaction(2018 FALL)/Child_Computer_Interaction_Project/AppData.db')
-    conn.execute('''CREATE TABLE Chore
-            (name text, time text,status text)''')
+    conn.execute('''CREATE TABLE if not exists Kids
+            (name text, time text)''')
     print ("Table created successfully")
     conn.close()
 
+def InsertNewKid():
+    conn = sqlite3.connect('AppData.db')
+ #   Kids_sql = "INSERT INTO Kids (name,age) VALUES (?,?)"
+    with conn:
+        cur = conn.cursor()
+        cur.execute("INSERT INTO Kids VALUES ('Colin','9')")
+        cur.execute("INSERT INTO Kids VALUES ('Peter','11')")
+        cur.execute("INSERT INTO Kids VALUES ('Jim','12')")
+    #print("Successfully insert "+ name + " into Kids Table")
+    conn.close()
+
+def where():
+    conn = sqlite3.connect('AppData.db')
+    with conn:
+        cur = conn.cursor()
+        cur.execute("SELECT name FROM Kids WHERE time = 11")
+        name = cur.fetchone()
+        print(name[0])
+'''
+def get():
+    conn = sqlite3.connect('AppData.db')
+    with conn:
+        cur = conn.cursor()
+        cur.execute("SELECT name from Kids")
+        rows = cur.fetchall()
+        for row in rows:
+            print(row[0])'''
+
 ConnectDataBase()
 CreateChoreTable()
+#InsertNewKid()
+where()
+
